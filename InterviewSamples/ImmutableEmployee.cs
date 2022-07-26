9018c1a3cda2b40171d0a2e689eb270d06d2bfe3
gmovila <gmovila@ozon.ru>
2022-07-26 23:11:58 +0200

    Add C#  tasks

---
+++ InterviewSamples/ImmutableEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewSamples
+{
+    public record ImmutableEmployee(string Name, string Title, string DepartmentName)
+    {
+        public string State { get; set; }
+        public string Name { get; set; } = Name;
+        public string Title { get; set; } = Title;
+        public string DepartmentName { get; set; } = DepartmentName;
+
+        public static IEnumerable<ImmutableEmployee> GetEmployees()
+        {
+            return new ImmutableEmployee[] {
+                new("John Doe", "Manager", "Sales"){ State="TX"},
+                new("Michael Scott", "Manager", "Sales"){ State="NY"},
+                new("Tom Hawk", "Sr Software Engineer", "Engineering"){ State="TX"},
+                new("Alicia Newton", "HR Manager", "Human Resources"){ State="TX"},
+                new("Alex Smith", "Software Engineer", "Engineering"){ State="NY"}
+            };
+        }
+
+        public virtual string[] GetACL()
+        {
+            return new string[] { "profile:read" };
+        }
+    }
+}
--- InterviewSamples/JavaScript/Task1.js
+++ InterviewSamples/JavaScript/Task1.js
@@ -3,25 +3,38 @@
 const people = [{ name: "John Doe", age: 40, state: "CA" }, { name: "Jane Doe", age: 25, state: "NY" }, { name: "Tom Cruise", age: 55, state: "CA" }];
 
 //states = [{name:"CA",averageAge: 47.5, people: ["John Doe", "Tom Cruise"] },{name:"NY",averageAge: 25, people: ["Jane Doe"] }];
-// function getStates(people) {....}
+function getStates(people) {
+    
+}
+
 
 console.log(people);
-
-
 
 
 
 //TASK 2
 //Write counter functions that can expose increment and decrement functions
 
-// let counter1 = new Counter(100);
-// let counter2 = new Counter(200);
+let Counter = function (number){
+    this.number = number;
+    
+    this.increment = function (){
+        return ++this.number;
+    }
+    
+    this.decrement = function (){
+        return --this.number;
+    }
+}
 
-// counter1.increment(); //-> 101
-// counter1.increment(); //-> 102
-// counter1.decrement(); //-> 101
-// counter2.increment(); //-> 201
-// counter1.increment(); //-> 102
+ let counter1 = new Counter(100);
+ let counter2 = new Counter(200);
+ 
+ counter1.increment(); //-> 101
+ counter1.increment(); //-> 102
+ counter1.decrement(); //-> 101
+ counter2.increment(); //-> 201
+ counter1.increment(); //-> 102
 
 
 
